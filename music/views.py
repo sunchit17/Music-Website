60e896da88c07842c4fb1b2f6bdca3a7ebf1eaf9
Sunchit <lakhanpalsunchit8@gmail.com>
2019-02-15 19:13:33 +0530

    Music Website,first commit

---
+++ music/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import Http404
+from .models import Album
+
+def index(request):
+    all_albums = Album.objects.all()
+    return render(request,'music/index.html',{"all_albums":all_albums})
+
+def detail(request,album_id):
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album Does not Exist")
+    return render(request,'music/detail.html',{"album":album})
